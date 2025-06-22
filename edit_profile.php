616f90d68cda2c18509f06e35940ff4e6b4b803a
edwin4ceo <1231203190@student.mmu.edu.my>
2025-06-22 17:11:17 +0800

    a

--- edit_profile.php
+++ edit_profile.php
@@ -102,6 +102,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile - BakeEase Bakery</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="edit-profile-styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="icon" href="images/logo.png" type="image/png" />
 </head>
