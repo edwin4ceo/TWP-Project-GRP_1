21dae5080f47b2ec2b5d622d742e023af6aa37fa
edwin4ceo <1231203190@student.mmu.edu.my>
2025-06-23 16:11:03 +0800

    a

--- contact.php
+++ contact.php
@@ -54,6 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us - BakeEase Bakery</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="about-contact-styles.css">
     <link rel="icon" href="images/logo.png" type="image/png" />
 </head>
 <body>
